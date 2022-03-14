81b782f74412e7490697d3ef4f4d15c4c8b905eb
Placer0884 <Erikpersson0884@gmail.com>
2022-03-14 20:55:11 +0100

    changed local repository

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./js/serviceWorker.js');
+  }
+}
+
+var nps = function () {
+    Main.py
+};
+
+$(".button").click(nps)
