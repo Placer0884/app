101c6b8443b2f398f99493f53fde64cb946f7b5b
Placer0884 <Erikpersson0884@gmail.com>
2022-04-03 01:59:49 +0200

    fixed the layout and started on the landscape mode

--- js/script.js
+++ js/script.js
@@ -8,27 +8,12 @@ window.onload = () =>
 
 // ---------------------------------------------------
 
-let nps = function() {
+
+
+let nps = function () {
     alert("You clicked a button");
 };
 
-$('button').click(nps)
 
 
-
-
-function startTime() {
-    const today = new Date();
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML = h + ":";
-    setTimeout(startTime, 1000);
-}
-
-function checkTime(i) {
-    if (i < 10) {i = "0" + i}
-    return i;
-}
+$("button").click(nps)
